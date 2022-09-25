ba0e0a7158d11edabe1317e4c984ca3e9f795049
Sutsikii <nicolas-dutertre@bbox.fr>
2022-09-25 15:50:14 +0200

    othermethod

--- webhook.php
+++ webhook.php
@@ -1,4 +1,4 @@
 <?php
-    $contents = file_get_contents('/home/sutsiki/Scripts/deploy.sh');
+    $contents = file_get_contents('/home/sutsiki/scripts/deploy.sh');
     echo shell_exec($contents);
     // echo shell_exec('sh /home/sutsiki/Scripts/deploy.sh');
